7da5e877a51b7585d20913ac9642c93d958d6f69
Mostafa Mosbah <mmosbah195@gmail.com>
2021-06-01 14:21:39 -0700

    added images folder

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content is added by JavaScript";
-document.querySelector('body').appendChild(h2);
+// const h2 = document.createElement('h2');
+// h2.textContent = "Photographer | Aspiring Software Developer";
+// document.querySelector('body').appendChild(h2);
 
-const p =document.createElement('p');
-p.textContent = "I am excited to get started on building my first website!";
-document.querySelector('body').appendChild(p);
+// const p =document.createElement('p');
+// p.textContent = "I am excited to get started on building my first website!";
+// document.querySelector('body').appendChild(p);
